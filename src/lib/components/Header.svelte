ba98959e6d48c189f944b6fc8f4793cee575d27c
paulbg <paulbg@hey.com>
2023-10-14 09:19:16 +0300

    feat(Header.svelte): add Header component to display a description of the book store
    The Header component is added to display a description of the book store. It includes an image and a text section. The image is set to a URL of a party image, and the text section provides a description of the book store, highlighting its collection and inviting readers to explore the world of literature.

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,34 @@
+<section>
+  <div
+    class="max-w-screen-xl px-4 py-8 mx-auto sm:py-12 sm:px-6 lg:py-16 lg:px-8"
+  >
+    <div class="grid grid-cols-1 gap-8 lg:grid-cols-2 lg:gap-16">
+      <div
+        class="relative h-64 overflow-hidden rounded-lg sm:h-80 lg:order-last lg:h-full"
+      >
+        <img
+          alt="Party"
+          src="https://images.pexels.com/photos/256455/pexels-photo-256455.jpeg?cs=srgb&dl=pexels-pixabay-256455.jpg&fm=jpg"
+          class="absolute inset-0 object-cover w-full h-full"
+        />
+      </div>
+
+      <div class="lg:py-24">
+        <h2 class="text-3xl font-bold sm:text-4xl">
+          Most famous book store in the US
+        </h2>
+
+        <p class="mt-4 text-gray-600">
+          Within the aisles of <strong class="italic font-bold"
+            >Bibliophile Heaven</strong
+          >, where tales from distant lands converge, every reader finds a story
+          that resonates. From age-old epics to contemporary narratives, our
+          collection promises a journey for each soul. Delve into the pages and
+          discover the tales that have stood the test of time, and the hidden
+          gems awaiting your discovery. The magic of literature at Bibliophile
+          Heaven beckons, inviting you into realms only bound by imagination.
+        </p>
+      </div>
+    </div>
+  </div>
+</section>
