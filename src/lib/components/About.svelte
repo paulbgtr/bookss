c2678d3656388d34df5e3646b6991afa4afbf1f4
paulbg <paulbg@hey.com>
2023-10-14 09:18:30 +0300

    feat(About.svelte): add About component to display information about Bibliophile Haven and its mission
    The About component is added to the project to provide a section on the website that introduces Bibliophile Haven and its purpose. The component includes a heading, an image, and a description of the bookstore's belief in the power of stories and the transformative nature of reading. This addition enhances the user experience by providing relevant information about the bookstore and its mission to ignite imagination through literature.

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,40 @@
+<section>
+  <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 sm:py-24 lg:px-8">
+    <div class="max-w-3xl">
+      <h2 class="text-3xl font-bold sm:text-4xl">
+        Welcome to
+        <strong class="p-2 bg-secondary">Bibliophile Haven</strong>, the gateway
+        to countless worlds.
+      </h2>
+    </div>
+
+    <div class="grid grid-cols-1 gap-8 mt-8 lg:grid-cols-2 lg:gap-16">
+      <div class="relative h-64 overflow-hidden sm:h-80 lg:h-full">
+        <img
+          alt="Bookstore"
+          src="https://st2.depositphotos.com/2769299/7314/i/450/depositphotos_73146775-stock-photo-a-stack-of-books-on.jpg"
+          class="absolute inset-0 object-cover w-full h-full"
+        />
+      </div>
+
+      <div class="lg:py-16">
+        <article class="space-y-4 text-gray-600">
+          <p>
+            We believe in the power of stories to transform lives. Whether
+            you're looking for an epic fantasy, a heartwarming romance, or a
+            gripping thriller, our shelves are filled with adventures waiting to
+            be discovered.
+          </p>
+
+          <p>
+            Each book is a portal, a magic door that takes you places you've
+            never been, introduces you to characters you've never met, and shows
+            you ideas you've never considered. From timeless classics to modern
+            tales, our selection is handpicked to ignite your imagination. Dive
+            in and lose yourself in the magic of reading.
+          </p>
+        </article>
+      </div>
+    </div>
+  </div>
+</section>
