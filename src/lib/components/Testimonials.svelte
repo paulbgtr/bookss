8664449e352661b72ddc4454d0c97c0266cd91cc
paulbg <paulbg@hey.com>
2023-10-14 09:19:55 +0300

    feat(Testimonials.svelte): add Testimonials component to display reader testimonials
    The Testimonials component is added to display reader testimonials about the collection of books. This component includes three testimonials with the name of the reader, their profile picture, and their feedback about the bookstore. The testimonials are displayed in a grid layout with a responsive design. This component enhances the user experience by showcasing positive feedback from readers, which can attract more customers to the bookstore.

---
+++ src/lib/components/Testimonials.svelte
@@ -0,0 +1,83 @@
+<section>
+  <div class="max-w-screen-xl px-4 py-12 mx-auto sm:px-6 lg:px-8 lg:py-16">
+    <h2
+      class="text-4xl font-bold tracking-tight text-center text-gray-900 sm:text-5xl"
+    >
+      Discover What Readers Say About Our Collection
+    </h2>
+
+    <div class="grid grid-cols-1 gap-4 mt-8 md:grid-cols-3 md:gap-8">
+      <blockquote class="p-6 rounded-lg shadow-sm bg-gray-50 sm:p-8">
+        <div class="flex items-center gap-4">
+          <img
+            alt="Reader"
+            src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+            class="object-cover rounded-full h-14 w-14"
+          />
+
+          <div>
+            <div class="flex justify-center gap-0.5 text-green-500">
+              <!-- star ratings are kept as they are universally recognized -->
+            </div>
+
+            <p class="mt-0.5 text-lg font-medium text-gray-900">Paul Starr</p>
+          </div>
+        </div>
+
+        <p class="mt-4 text-gray-700">
+          "The selection here is unparalleled. I've discovered titles I never
+          knew I needed. A bookworm's haven!"
+        </p>
+      </blockquote>
+
+      <blockquote class="p-6 rounded-lg shadow-sm bg-gray-50 sm:p-8">
+        <div class="flex items-center gap-4">
+          <img
+            alt="Reader"
+            src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+            class="object-cover rounded-full h-14 w-14"
+          />
+
+          <div>
+            <div class="flex justify-center gap-0.5 text-green-500">
+              <!-- star ratings are kept as they are universally recognized -->
+            </div>
+
+            <p class="mt-0.5 text-lg font-medium text-gray-900">Alexa Reed</p>
+          </div>
+        </div>
+
+        <p class="mt-4 text-gray-700">
+          "Every visit to this store is a journey. I always leave with a gem
+          that becomes a favorite read."
+        </p>
+      </blockquote>
+
+      <blockquote class="p-6 rounded-lg shadow-sm bg-gray-50 sm:p-8">
+        <div class="flex items-center gap-4">
+          <img
+            alt="Reader"
+            src="https://images.unsplash.com/photo-1595152772835-219674b2a8a6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1180&q=80"
+            class="object-cover rounded-full h-14 w-14"
+          />
+
+          <div>
+            <div class="flex justify-center gap-0.5 text-green-500">
+              <!-- star ratings are kept as they are universally recognized -->
+            </div>
+
+            <p class="mt-0.5 text-lg font-medium text-gray-900">
+              Jordan Matthews
+            </p>
+          </div>
+        </div>
+
+        <p class="mt-4 text-gray-700">
+          "I've been to many bookstores, but this one has a unique charm. The
+          collection is both vast and curated, ensuring you find what you seek
+          and more!"
+        </p>
+      </blockquote>
+    </div>
+  </div>
+</section>
